d031114d00d59ca131b3399b880508f2124520aa
Ali-Akber Saifee <ali@indydevs.org>
2015-04-19 07:38:08 +0800

    Add coverage to travis tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,10 @@
 import glob
 import sys
 import os
+import shutil
 version = sys.version.split(" ")[0]
 majorminor = version[0:3]
-
 paths = glob.glob("build/lib*-%s" % majorminor)
+out_dir = os.path.join(os.path.dirname(os.path.split(__file__)[0]), 'pycrm114')
 if paths:
-    sys.path.insert(0, os.path.abspath(paths[0]))
+    shutil.copy(os.path.join(paths[0], 'pycrm114', '_binding.so'), out_dir)
